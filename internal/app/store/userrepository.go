14414b1fc6392bd2f8b3c858c0fb2e8549477fa1
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2024-11-21 17:52:35 +0500

    Part 2 (no end)

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -7,7 +7,15 @@ type UserRepository struct {
 }
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
-	return nil, nil
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES($1, $2) RETURNING id"
+		u.Email,
+		u.EncryptedPassword
+		).Scan(&u.ID); err!=nil{
+		return nil, err
+	}
+	
+	return u, nil
 }
 
 func (r *UserRepository) FindEmail(email string) (*model.User, error) {
