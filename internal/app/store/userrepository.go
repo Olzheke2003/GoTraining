976193158389193c84ff0415882d92e3e59090ba
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2024-12-04 15:34:46 +0500

    userRepository and store test bd

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -8,13 +8,13 @@ type UserRepository struct {
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
 	if err := r.store.db.QueryRow(
-		"INSERT INTO users (email, encrypted_password) VALUES($1, $2) RETURNING id"
+		"INSERT INTO users (email, encrypted_password) VALUES($1, $2) RETURNING id",
 		u.Email,
-		u.EncryptedPassword
-		).Scan(&u.ID); err!=nil{
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
 		return nil, err
 	}
-	
+
 	return u, nil
 }
 
